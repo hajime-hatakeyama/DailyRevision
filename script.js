a52265feaada913e67beb4e50699206714ae3dcb
hajime hatakeyama <63620706+hajime-hatakeyama@users.noreply.github.com>
2020-04-30 08:49:54 +0900

    special

---
+++ script.js
@@ -0,0 +1,35 @@
+$(function () {
+  const $change = $("#change-css");
+  const $text = $("#text");
+
+  $change.click(function () {
+    document.getElementById("papa2").src = "img/20200419hanu2.PNG";
+    document.getElementById("mama2").src = "img/20200419sei2.PNG";
+    document.getElementById("meru2").src = "img/20200419meru5.PNG";
+    document.getElementById("runa2").src = "img/runaIntro2.PNG";
+    // document.getElementById("text").href = "http://google.co.jp";
+    $change.html("登録済み");
+    // document.getElementById($text).innerHTML(
+    //   <h1>
+    //     <a href="movie/runa20200422.mp4">"ようこそ！畠山わんこ一家へ!")</a>
+    //   </h1>
+    // );
+    $text.html("ボタンを押した後<br>もう一度写真を見てみて!");
+    $text.css("color", "red");
+    $text.css("font-size", "30px");
+    $text.css("text-decoration", "underline");
+    // document.getElementById("text").href = "http://google.co.jp";
+    // $text.fadeOut(2000);
+  });
+
+  $(function () {
+    $(".lesson-icon").hover(
+      function () {
+        $(".txt-contents").fadeIn();
+      },
+      function () {
+        $(".txt-contents").fadeOut();
+      }
+    );
+  });
+});
