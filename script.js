4a06f5c98e41d7cce8877649b4b673f544361f15
hajime hatakeyama <63620706+hajime-hatakeyama@users.noreply.github.com>
2020-04-30 08:28:24 +0900

    new

--- script.js
+++ script.js
@@ -8,10 +8,18 @@ $(function () {
     document.getElementById("mama2").src = "img/20200419sei2.PNG";
     document.getElementById("meru2").src = "img/20200419meru5.PNG";
     document.getElementById("runa2").src = "img/runaIntro2.PNG";
+    // document.getElementById("text").href = "http://google.co.jp";
     $change.html("登録済み");
+    // document.getElementById($text).innerHTML(
+    //   <h1>
+    //     <a href="movie/runa20200422.mp4">"ようこそ！畠山わんこ一家へ!")</a>
+    //   </h1>
+    // );
+    $text.html("ようこそ！畠山わんこ一家へ!");
     $text.css("color", "red");
     $text.css("font-size", "30px");
-    $text.html("ようこそ！畠山わんこ一家へ!");
-    $text.fadeOut(2000);
+    $text.css("text-decoration", "underline");
+    // document.getElementById("text").href = "http://google.co.jp";
+    // $text.fadeOut(2000);
   });
 });
