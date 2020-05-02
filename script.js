b3f0df7ddb5438ac1cb3ba3bd962df207ae3bbeb
hajime hatakeyama <63620706+hajime-hatakeyama@users.noreply.github.com>
2020-05-02 09:06:08 +0900

    commit

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ $(function () {
     document.getElementById("mama2").src = "img/20200419sei2.PNG";
     document.getElementById("meru2").src = "img/20200419meru5.PNG";
     document.getElementById("runa2").src = "img/runaIntro2.PNG";
-   
+
     $change.html("登録済み");
- 
+
     $text.html("ボタンを押した後、もう一度写真を見てみて!");
     $text.css("color", "red");
     $text.css("font-size", "30px");
-    $text.css("text-decoration", "underline");  
+    $text.css("text-decoration", "underline");
   });
   // メッセージを隠しておく処理
   $(function () {
@@ -27,17 +27,38 @@ $(function () {
       }
     );
   });
+  // $(".lesson-hover").hover(
+  //   function () {
+  //     $(this).find(".text-contents").addClass("text-active");
+  //   },
+  //   function () {
+  //     $(this).find(".text-contents").removeClass("text-active");
+  //   }
+  // );
   // モーダルの処理
-  $('#login-show').click(function(){
-    $('#login-modal').fadeIn();
+  $("#login-show").click(function () {
+    $("#login-modal").fadeIn();
   });
-  
-  $('.signup-show').click(function(){
-    $('#signup-modal').fadeIn();
-  });  
 
-  $('.close-modal').click(function() {
-    $('#login-modal').fadeOut();
-    $('#signup-modal').fadeOut();
+  $(".signup-show").click(function () {
+    $("#signup-modal").fadeIn();
+  });
+
+  $(".close-modal").click(function () {
+    $("#login-modal").fadeOut();
+    $("#signup-modal").fadeOut();
+  });
+  // アコーディオンの処理
+  $(".faq-list-item").click(function () {
+    const $answer = $(this).find(".answer");
+    if ($answer.hasClass("open")) {
+      $answer.removeClass("open");
+      $answer.slideUp();
+      $(this).find("span").text("+");
+    } else {
+      $answer.addClass("open");
+      $answer.slideDown();
+      $(this).find("span").text("-");
+    }
   });
 });
